62a262a618bbd2b647326514f6c0014b7360d02e
Eden-N-H <eden.hallett2005@gmail.com>
2026-09-11 11:00:18 +1000

    Initial commit

---
+++ MacroEasy/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MacroEasy
+//
+//  Created by Eden Hallett on 11/9/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MacroEasyTests/MacroEasyTests.swift
@@ -0,0 +1,19 @@
+//
+//  MacroEasyTests.swift
+//  MacroEasyTests
+//
+//  Created by Eden Hallett on 11/9/2026.
+//
+
+import Testing
+@testable import MacroEasy
+
+struct MacroEasyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
